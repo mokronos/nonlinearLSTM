d3760995f0769eff03c0ed4fd6cd1bebe353c611
Mokronos <sebastian@hirts.de>
2022-06-19 18:12:34 +0200

    updated

--- ode.py
+++ ode.py
@@ -13,18 +13,6 @@ from torch.utils.data import Dataset
 # --> y[0]' = y[1]
 # --> y[1]' = - (g/l) * sin(y[0])
 
-def model(t,y):
-    l = 1
-    g = 9.8
-
-    res = [y[1], -(g/l) * np.sin(y[0])]
-    return res
-
-# [angle'_init, angle_init]
-y0 = [0, 0.1]
-samples = 500
-timestep = 0.01
-t = np.arange(0, samples*timestep, timestep)
 
 # y = solve_ivp(model, [0,samples*timestep], y0, dense_output=True, method = "LSODA")
 # y = y.sol(t)
@@ -36,28 +24,41 @@ t = np.arange(0, samples*timestep, timestep)
 
 # plt.show()
 
+class timeseries(Dataset):
+
+    def __init__(self, x, y):
+        self.x = torch.tensor(x)
+        self.y = torch.tensor(y)
+        self.len = x.shape[0]
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+
+    def __len__(self):
+        return self.len
+
+
 class Gen:
     """
     Generator for input and output data from a given differential equation
     """
 
-    def __init__(self, func, parameters, y0, timestep, samples):
+    def __init__(self, func, parameters, y0, dt, samples):
 
         self.func = func
         self.parameters = parameters
         self.y0 = y0
-        self.timestep = timestep
+        self.dt = dt
         self.samples = samples
         self.rng = np.random.default_rng(seed=42)
     
     def step(self): 
         
-        t = np.arange(0, self.timestep*2, self.timestep)
-        result = solve_ivp(self.func, [0, self.timestep*2], self.y0, dense_output=True, method = "LSODA")
-        result = result.sol(t)
-        result = result[:,-1]
-        self.y0 = result
-        return result
+        # t = np.arange(0, self.dt*2, self.dt)
+        result = solve_ivp(self.func, [0, self.dt], self.y0, method = "LSODA", t_eval = [self.dt])
+        # result = result.sol(t)
+        self.y0 = result.y[:,0]
+        return result.y[:,0]
 
     def generate(self):
         x = []
@@ -66,10 +67,12 @@ class Gen:
 
         x = np.array(x)
         # x = self.add_noise(x)
-        X,y = self.gen_samples(x)
-        data = timeseries(X, y)
+
+        return x
+        # X,y = self.gen_samples(x)
         
-        return data
+        # return X,y
+
 
     def add_noise(self, data):
 
@@ -94,29 +97,28 @@ class Gen:
         return np.array(X), np.array(y)
 
 
-class timeseries(Dataset):
+def model(t,y):
+    l = 1
+    g = 9.8
 
-    def __init__(self, x, y):
-        self.x = torch.tensor(x)
-        self.y = torch.tensor(y)
-        self.len = x.shape[0]
+    res = [y[1], -(g/l) * np.sin(y[0])]
+    return res
 
-    def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
-
-    def __len__(self):
-        return self.len
-
-
+# [angle'_init, angle_init]
+y0 = [0, 0.1]
+samples = 500
+timestep = 0.01
+t = np.arange(0, samples*timestep, timestep)
 
 x = Gen(model,2,y0,timestep,samples)
 
-data = x.generate()
-t = np.arange(0, len(data)*5)
+# X, y = x.generate()
+X = x.generate()
 
-for i, row in enumerate(data):
-    print(i)
-    plt.plot(t[i*5 : (i+1) *5], row[0])
+plt.plot(t, X)
+
+# for i, row in enumerate(X):
+#     plt.plot(t[i*5:(i+1)*5], row)
 
 plt.show()
 # t = np.arange(0, samples*timestep, timestep)
