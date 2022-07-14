5a4d8c9d47d981ccca21decae4216a55cbafc360
mokronos <sebastian@hirts.de>
2022-07-14 08:24:13 +0200

    cleaned up and tried simple pendulum

--- main.py
+++ main.py
@@ -1,92 +1,46 @@
-from ode import Gen
-
 import numpy as np
-import matplotlib.pyplot as plt
-
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-from torch.autograd import Variable 
+from data import Gen
 
+import matplotlib.pyplot as plt
 
-# X = np.array([[[1],[2],[3],[4]]])
-# y = np.array([[[2],[3],[4],[5]]])
+# set random seed
+torch.manual_seed(3)
 
-max = 20
-data = np.array(range(max))
+#################################################
+# Dataset custom class 
+class BasicDataset(Dataset):
 
-# make data into shape (samples, timeseries_length, num_features)
-data = np.reshape(data, (1,max,1))
-X = data[:,:-1,:]
-y = data[:,1:,:]
+    def __init__(self, X, y):
 
-train_test_split = 0.8
-cutoff = int(data.shape[1] * (train_test_split))
-# with more data there will probably be a train test split of the sequences, not the samples in the sequence
-train_X, train_y = X[:,:cutoff], y[:,:cutoff]
-test_X, test_y = X[:,cutoff:], y[:,cutoff:]
-
-class timeseries(Dataset):
-
-    def __init__(self, x, y):
-        self.x = torch.tensor(x, dtype=torch.float32)
+        self.X = torch.tensor(X, dtype=torch.float32)
         self.y = torch.tensor(y, dtype=torch.float32)
-        self.len = x.shape[0]
-
-    def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
 
     def __len__(self):
-        return self.len
 
-batch_size = 1
+        return len(self.X)
 
-training_data = timeseries(train_X, train_y)
-test_data = timeseries(test_X, test_y)
+    def __getitem__(self, index):
 
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+        return self.X[index], self.y[index]
 
-for X, y in train_dataloader:
-    print(X)
-    print(y)
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
 
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-# Define model
-class LSTMNet(nn.Module):
-    def __init__(self):
-        super(LSTMNet, self).__init__()
-        self.hidden_size = 10
-        self.batch_size = 1
-        self.lstm = nn.LSTM(1,self.hidden_size, batch_first=True)
-        # self.ch = (torch.zeros(1,self.batch_size, self.hidden_size, device=device), torch.zeros(1,self.batch_size, self.hidden_size, device=device))
-        self.fc = nn.Linear(self.hidden_size,128)
-        self.fc1 = nn.Linear(128,1)
-        self.relu = nn.ReLU()
+# NN architecture
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size,output_size)
 
     def forward(self, x):
+        x,_ = self.lstm(x)
+        x = self.fc(x)
+        return x
 
-        output, _ = self.lstm(x)
-        output = self.relu(output)
-        output = self.fc(output)
-        output = self.relu(output)
-        output = self.fc1(output)
-        return output
-
-model = LSTMNet().to(device)
-print(model)
-
-loss_fn = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+#################################################
+# define training and test loops (pretty much default from pytorch quickstart)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -102,40 +56,134 @@ def train(dataloader, model, loss_fn, optimizer):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        loss_vals.append(loss.item())
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            # print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        # for weight in model.named_parameters():
+        #     print(f"{weight}")
+
+        # print(f"loss: {loss.item()}")
 
 
 def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss = 0
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
-            print(X)
-            print(y)
-            print(pred)
-            p = pred.to("cpu")
-            yp = y.to("cpu")
-            plt.plot(p.flatten())
-            plt.plot(yp.flatten())
+            # test_results.append(pred.cpu())
+            # change this, its ugly
+            test_results.append(pred.cpu())
+            # print(X,y)
+            # print(pred)
             test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n , Avg loss: {test_loss:>8f} \n")
 
 
-epochs = 50000
+#################################################
+# create training and test data
+
+
+# X 1s --> y time dependent but linear
+# y = np.array(list(range(1,10)))
+# X = np.array([1]*len(y))
+# y = y.reshape((1,len(y),1))
+# X = X.reshape((1,len(X),1))
+
+# X = np.array(list(range(1,10)))
+# X = X.reshape((1,len(X),1))
+# y = X
+
+def func(y,t,u,g):
+    l = 1
+
+    res = [y[1], -(g/l) * np.sin(y[0]) + u[0] + u[1]]
+    return res
+
+# [angle'_init, angle_init]
+y0 = [0, 0.1]
+samples = 500
+dt = 0.01
+
+# if parameters should vary in time, give them as lists like this for [[par1(t=0),par2(t=0)], [par1(t=1),par2(t=1)], ... ]; just append them as list, then transpose
+# if single constant, just give them as one value
+u = []
+u.append([0]*samples)
+u.append([1]*samples)
+u = np.array(u)
+u = u.T
+g = 9.81
+
+# give input, then parameters (both as tuples); inputs = things the RNN/model gets as input as well, parameters = things the model is supposed to learn (potential changes in the system)
+x = Gen(func,(u,),(g,),y0,dt,samples)
+x.generate()
+x.transform()
+
+
+X = np.expand_dims(x.X, axis=0)
+y = np.expand_dims(x.y, axis=0)
+
+cutoff = int(X.shape[1] * 2/3 )
+X_train, y_train = X[:,:cutoff], y[:,:cutoff]
+X_test, y_test = X[:,cutoff:], y[:,cutoff:]
+# X_train, y_train = X[:,:6], y[:,:6]
+# X_test, y_test = X[:,:6], y[:,:6]
+# print(X_train)
+
+training_data = BasicDataset(X_train, y_train)
+test_data = BasicDataset(X_test, y_test)
+
+batch_size = 64
+
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+#################################################
+# create NN and train
+
+global device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cpu"
+print(f"Using {device} device")
+
+# number of features
+input_size = X_train.shape[2]
+# whatever is good? to be determined
+hidden_size = 20
+# number of outputs
+output_size = y_train.shape[2]
+
+model = NeuralNetwork(input_size,hidden_size,output_size).to(device)
+print(model)
+
+epochs = 1000
+lr = 1e-1
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+# some lists for results
+global loss_vals
+loss_vals = []
+global test_results
+test_results = []
+
 for t in range(epochs):
-    if t%10000 == 0:
+    
+    if t%100==0:
         print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
+
 test(test_dataloader, model, loss_fn)
+
 print("Done!")
-# plt.show()
+
+#################################################
+# plot results
+
+_,ax = plt.subplots(3)
+ax[0].plot(loss_vals)
+ax[1].plot(y_test[0])
+ax[2].plot(test_results[0][0])
+plt.show()
